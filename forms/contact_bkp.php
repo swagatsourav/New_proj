c02ed2e746ec5fc27ad5be0687ff52c72027ff8e
Swagat <swagat.miku@gmail.com>
2020-07-07 09:14:12 +0530

    removed user name and passwd.

--- forms/contact_bkp.php
+++ forms/contact_bkp.php
@@ -27,8 +27,8 @@
 
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
-    'username' => 'swagat.miku@gmail.com',
-    'password' => 'miku@123',
+    'username' => '',
+    'password' => '',
     'port' => '587'
   );
 
